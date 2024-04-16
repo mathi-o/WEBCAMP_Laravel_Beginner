a5a3bb4a99470aa5cc42c3bae11cb0bfabd740ea
masato matsuoka <masato.matsuoka@example.com>
2024-04-16 04:54:52 +0000

    非ログイントップページ作成

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller{
+    /**
+     * トップページを表示する
+     *
+     * @return \Illuminate\view\view
+     */
+    public function index(){
+
+        return view('index');
+    }
+    /**
+     * 2nd ページを表示
+     *
+     * @return \Illuminate\View\View
+     */
+
+
+
+}
+
+
+
